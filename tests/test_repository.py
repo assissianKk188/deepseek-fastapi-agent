18f82c08c59ea07324fcb9e6e5c138d4ccd3745a
assissianKk188 <142670718+assissianKk188@users.noreply.github.com>
2026-08-26 18:03:25 +0800

    feat: add tests/test_repository.py

---
+++ tests/test_repository.py
@@ -0,0 +1,10 @@
+from deepseek_fastapi_agent.repository import InMemoryConversationRepository
+
+
+def test_repository_appends_and_returns_history() -> None:
+    repository = InMemoryConversationRepository()
+
+    repository.append("c-1", "user", "你好")
+
+    assert repository.history("c-1") == [("user", "你好")]
+
